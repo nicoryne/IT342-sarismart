18123a087bc8f96ead90864520d3499960322666
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-09 12:15:20 +0800

    refactoring to compose

--- frontend_mobile/app/build.gradle.kts
+++ frontend_mobile/app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.androidApplication)
+    alias(libs.plugins.compose.compiler)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.kapt")
@@ -31,7 +32,9 @@ android {
         }
     }
     buildFeatures {
-        dataBinding = true
+        compose = true
+        dataBinding = false
+        viewBinding = false
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -46,6 +49,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
@@ -56,6 +60,33 @@ dependencies {
     // Dagger Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Hilt + ViewModel
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Jetpack Compose
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Material 3
+    implementation(libs.androidx.material3)
+
+    // Android UI
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Material Icons
+    implementation(libs.androidx.material.icons.extended)
+
+    // Google Font
+    implementation(libs.androidx.ui.text.google.fonts)
+
 }
 
 kapt {
--- frontend_mobile/build.gradle.kts
+++ frontend_mobile/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.compose.compiler) apply false
     id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
     id("com.google.dagger.hilt.android") version "2.56.1" apply false
 }
