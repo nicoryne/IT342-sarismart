f3240ab6c4c187c3ac2dc5c8ad31fff651b0dbc0
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-09 15:22:17 +0800

    removed .gradle and .kotlin folders

--- frontend_mobile/app/src/main/java/edu/cit/sarismart/ui/MainScreen.kt
+++ frontend_mobile/app/src/main/java/edu/cit/sarismart/ui/MainScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
