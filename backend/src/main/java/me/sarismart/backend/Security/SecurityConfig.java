1b66326f00727023feddd5456fb8bb3e8306e47a
John Kenny Quijote <johnkenny.quijote@cit.edu>
2025-04-18 23:23:40 +0800

    fix: update CORS configuration to allow requests from the production URL

--- backend/src/main/java/me/sarismart/backend/Security/SecurityConfig.java
+++ backend/src/main/java/me/sarismart/backend/Security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://sarismart-backend.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
