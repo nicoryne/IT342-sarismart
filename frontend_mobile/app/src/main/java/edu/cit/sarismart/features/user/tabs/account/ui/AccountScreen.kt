c06dd7c8a1ce81328dc533c3dca06af2aa53340b
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-19 19:59:22 +0800

    feat: cleaned up ui for account

--- frontend_mobile/app/src/main/java/edu/cit/sarismart/features/user/tabs/account/ui/AccountScreen.kt
+++ frontend_mobile/app/src/main/java/edu/cit/sarismart/features/user/tabs/account/ui/AccountScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
@@ -17,11 +19,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import edu.cit.sarismart.features.user.components.Header
+import edu.cit.sarismart.features.user.tabs.account.ui.components.UserDetailBox
 
-@Composable
+    @Composable
 fun AccountScreen (
     onNavigateToLogin: () -> Unit,
     onNavigateToNotifications: () -> Unit,
@@ -50,12 +54,46 @@ fun AccountScreen (
 
             Header("My Account", onNavigateToNotifications)
 
+            Spacer(modifier = Modifier.height(64.dp))
+
             Column (
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
+                Text(
+                    text = "Hello, Nicolo",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.primary,
+
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                UserDetailBox(
+                    label = "Full Name",
+                    value = "Nicolo",
+                    isEditable = true,
+                    onEditClick = { /* TODO */}
+                )
+
+                UserDetailBox(
+                    label = "Email",
+                    value = "rynedev.ph@gmail.com",
+                    isEditable = false,
+                    onEditClick = { /* TODO */}
+                )
+
+                UserDetailBox(
+                    label = "Phone Number",
+                    value = "+63 993 894 8592",
+                    isEditable = true,
+                    onEditClick = { /* TODO */}
+                )
             }
 
             OutlinedButton(
