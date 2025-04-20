a494751db8d724dc75c192ff0a4bd845207c9a24
John Kenny Quijote <johnkenny.quijote@cit.edu>
2025-04-20 13:43:28 +0800

    feat: log JWT_SECRET_KEY from environment and update secret key configuration in application properties

--- backend/src/main/java/me/sarismart/backend/SarismartBackendApiApplication.java
+++ backend/src/main/java/me/sarismart/backend/SarismartBackendApiApplication.java
@@ -12,6 +12,10 @@ public class SarismartBackendApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SarismartBackendApiApplication.class, args);
+	
+		// Log the JWT_SECRET_KEY environment variable
+        String jwtSecretKey = System.getenv("JWT_SECRET_KEY");
+        System.out.println("JWT_SECRET_KEY from environment: " + jwtSecretKey);
 	}
 
 }
