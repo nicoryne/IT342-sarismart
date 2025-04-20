8f9c81aa68831bd9cc374d381b0f33f8bdafed1e
John Kenny Quijote <johnkenny.quijote@cit.edu>
2025-04-20 14:09:21 +0800

    refactor: remove unused configuration properties and streamline AppConfig

--- backend/src/main/java/me/sarismart/backend/SarismartBackendApiApplication.java
+++ backend/src/main/java/me/sarismart/backend/SarismartBackendApiApplication.java
@@ -2,20 +2,12 @@ package me.sarismart.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import me.sarismart.backend.Config.AppConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppConfig.class)
 public class SarismartBackendApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SarismartBackendApiApplication.class, args);
-	
-		// Log the JWT_SECRET_KEY environment variable
-        String jwtSecretKey = System.getenv("JWT_SECRET_KEY");
-        System.out.println("JWT_SECRET_KEY from environment: " + jwtSecretKey);
 	}
 
 }
