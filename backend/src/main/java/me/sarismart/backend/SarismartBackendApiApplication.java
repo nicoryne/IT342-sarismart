fe018e63f06a8d9c2189f4a500cf6b7bc26e2465
John Kenny Quijote <johnkenny.quijote@cit.edu>
2025-04-20 13:16:53 +0800

    fix: enable configuration properties for AppConfig in the main application class

--- backend/src/main/java/me/sarismart/backend/SarismartBackendApiApplication.java
+++ backend/src/main/java/me/sarismart/backend/SarismartBackendApiApplication.java
@@ -2,8 +2,12 @@ package me.sarismart.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import me.sarismart.backend.Config.AppConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppConfig.class)
 public class SarismartBackendApiApplication {
 
 	public static void main(String[] args) {
